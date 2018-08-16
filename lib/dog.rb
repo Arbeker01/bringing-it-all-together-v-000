faa3241d4ba7701cc4dc35fdbb7b86f34779ab99
Romeo Gono <romeogono1@aol.com>
2018-08-16 03:08:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,20 +24,29 @@ class Dog
 
    DB[:conn].execute(sql)
   end
-  
+
   def self.new_from_db(row)
    dog = self.new(name: row[1], breed: row[2], id: row[0])
    dog
   end
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * 
-      FROM dogs 
+      SELECT *
+      FROM dogs
       WHERE name = ?
     SQL
 
     result = DB[:conn].execute(sql, name)[0]
     Dog.new(id: result[0], name: result[1], breed: result[2])
-  end
+   end
+   
+   def update
+    sql = <<-SQL
+      UPDATE dogs 
+      SET name = ?, breed = ? 
+      WHERE id = ?
+    SQL
 
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 end
